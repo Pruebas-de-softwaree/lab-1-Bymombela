b118cd6ba96b5381168447bd23fe023ab539c568
Bymombela <156949349+Bymombela@users.noreply.github.com>
2025-08-29 03:35:58 +0000

    RFN1

--- user_manager.py
+++ user_manager.py
@@ -28,20 +28,24 @@ class UserManager:
 
 if __name__ == "__main__":
     user_manager = UserManager()
-    for i in range(100):
-        user_manager.add_user(i,f"Yo soy el num : {i}")
-    print("end")
-    for i in range (100):
-        print(user_manager.find_user(i))
+    # for i in range(100):
+    #     user_manager.add_user(i,f"Yo soy el num : {i}")
+    # print("end")
+    # for i in range (100):
+    #     print(user_manager.find_user(i))
 
     # for i in range (500):
     #     user_manager.delete_user(i)
     #     print("El usuario fue eliminado:",i)
 
-    nombre = user_manager.get_all_names()
-    print(f"{nombre}")
+    # nombre = user_manager.get_all_names()
+    # print(f"{nombre}")
 
-    promedio = user_manager.average_user_id()
-    print ("El promedio es",promedio)
+    # promedio = user_manager.average_user_id()
+    # print ("El promedio es",promedio)
+
+    for i in range (1000):
+        user_manager.add_user (i, f"Soy el usuario agregado numero:{i}")
+    print (user_manager.get_all_names())
 
 
